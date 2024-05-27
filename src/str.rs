33d2757eb390c800915d6f4c3cff623636a20c18
PMC <pandamancoin@gmail.com>
2024-05-27 17:47:20 +0800

    is_not_blank

--- src/str.rs
+++ src/str.rs
@@ -1,6 +1,15 @@
 
+/// is_blank "" == s.trim()
 pub fn is_blank(s: String) -> bool {
     if "" == s.trim() {
+        return true;
+    }
+    false
+}
+
+/// is_not_blank "" != s.trim()
+pub fn is_not_blank(s: String) -> bool {
+    if "" != s.trim() {
         return true;
     }
     false
