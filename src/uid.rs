0189ae14f1945be93c36e1b5375a640026261de6
PMC <pandamancoin@gmail.com>
2024-05-28 17:46:47 +0800

    uid

--- src/uid.rs
+++ src/uid.rs
@@ -1,6 +1,13 @@
 use uuid::Uuid;
 
+/// uid_v4 uid v4 不带 -
 pub fn uid_v4() -> String {
+    let u = Uuid::new_v4();
+    return u.to_string().replace("-", "");
+}
+
+pub fn uid_v4_original() -> String {
     let u = Uuid::new_v4();
     return u.to_string();
 }
+
